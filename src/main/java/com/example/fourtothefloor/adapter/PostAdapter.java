f68cfb01d5a096b8c63113cdc6a5dd4fd2c3e5fc
Dean Mulrooney <mulrooney.dean@gmail.com>
2020-04-20 13:46:09 +0100

    Post Adapter and post_view added

---
+++ src/main/java/com/example/fourtothefloor/adapter/PostAdapter.java
@@ -0,0 +1,83 @@
+package com.example.fourtothefloor.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fourtothefloor.R;
+import com.example.fourtothefloor.model.PostModel;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+
+    Context context;
+    List<PostModel> postModels;
+
+    public PostAdapter(Context context, List<PostModel> postModels) {
+        this.context = context;
+        this.postModels = postModels;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return postModels.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.people_image)
+        ImageView peopleImage;
+        @BindView(R.id.people_name)
+        TextView peopleName;
+        @BindView(R.id.date)
+        TextView date;
+        @BindView(R.id.privacy_icon)
+        ImageView privacyIcon;
+        @BindView(R.id.memory_meta_rel)
+        RelativeLayout memoryMetaRel;
+        @BindView(R.id.post)
+        TextView post;
+        @BindView(R.id.status_image)
+        ImageView statusImage;
+        @BindView(R.id.like_img)
+        ImageView likeImg;
+        @BindView(R.id.like_txt)
+        TextView likeTxt;
+        @BindView(R.id.likeSection)
+        LinearLayout likeSection;
+        @BindView(R.id.comment_img)
+        ImageView commentImg;
+        @BindView(R.id.comment_txt)
+        TextView commentTxt;
+        @BindView(R.id.commentSection)
+        LinearLayout commentSection;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
--- src/main/java/com/example/fourtothefloor/fragment/NewsFeedFragment.java
+++ src/main/java/com/example/fourtothefloor/fragment/NewsFeedFragment.java
@@ -5,18 +5,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fourtothefloor.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class NewsFeedFragment extends Fragment {
 
     Context context;
+    @BindView(R.id.newsfeed)
+    RecyclerView newsfeed;
+    @BindView(R.id.newsfeedProgressBar)
+    ProgressBar newsfeedProgressBar;
+    Unbinder unbinder;
+
     @Override
-    public void onAttach(Context context){
+    public void onAttach(Context context) {
         super.onAttach(context);
         this.context = context;
 
@@ -25,8 +38,17 @@ public class NewsFeedFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_newsfeed,container,false);
+        View view = inflater.inflate(R.layout.fragment_newsfeed, container, false);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context);
+        newsfeed.setLayoutManager(linearLayoutManager);
 
+        unbinder = ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
